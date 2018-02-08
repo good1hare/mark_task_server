f71ee0ac3a7550bde97576472fe184bb1b2b1bf7
Bulat Sharifullin <good1hare@gmail.com>
2018-02-08 13:15:47 +0300

    my sql

--- app/routes/index.js
+++
@@ -1,5 +0,0 @@
-const taskRoutes = require('./task_routes');
-module.exports = function(app, db) {
-  taskRoutes(app, db);
-  // Тут, позже, будут и другие обработчики маршрутов 
-};
--- server.js
+++ server.js
@@ -1,17 +1,37 @@
 const express        = require('express');
-const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const db             = require('./config/db');
 const app            = express();
 const port = 8000;
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'dbuser',
+  password : 's3kreee7'
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-MongoClient.connect(db.url, (err, database) => {
-  if (err) return console.log(err)
-  require('./app/routes')(app, database);
-  app.listen(port, () => {
-  	var now = new Date();
-    console.log('We are live on ' + port + now);
-  });
-})
+app.listen(port, () => {
+	var now = new Date();
+  console.log('Start server ' + port + " " + now);
+});
+
+app.post('/create_tasks', (req, res) => {
+	connection.connect();
+	connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+	  if (err) throw err;
+	  console.log('The solution is: ', rows[0].solution);
+	});
+
+connection.end();
+  console.log(req.body)
+  const task = { text: req.body.body, title: req.body.title}
+	// db.collection('tasks').insert(task, (err, results) => {
+	// 	if (err) {
+  //     	res.send({ 'error': 'An error has occurred' });
+  //   	} else {
+  //     	res.send(result.ops[0]);
+  //   	}
+	res.send("Hello");
+	});
