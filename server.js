c0a1c10db4738f2a83e69ae66aa03267256aca80
Bulat Sharifullin <good1hare@gmail.com>
2018-05-12 14:44:30 +0300

    2

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ app.post('/auth', (req, res) => {
 	console.log(req.body)
   const user = { login: req.body.login, password: req.body.password}
 
-	connection.query("SELECT * FROM mark_task.users WHERE user_email ''" + user.login + "'';", function(err, rows, fields) {
-		console.log(rows, fields)
+	connection.query("SELECT * FROM mark_task.users WHERE user_email='" + user.login + "';", function(err, rows, fields) {
+		console.log(rows, fields, err)
 	});
 
 	var userInfo = {
