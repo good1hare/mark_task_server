b8058e1f9e443172c040a52487a3885e8dc20226
Bulat Sharifullin <good1hare@gmail.com>
2018-05-12 14:00:05 +0300

    ura

--- server.js
+++ server.js
@@ -47,9 +47,7 @@ app.post('/reg', (req, res) => {
 	const user = { email: req.body.mail, password: req.body.password, name: req.body.name}
 
 	connection.query("insert into users (user_name, user_email, user_password) values ('" + user.name + "','" + user.email + "','" + user.password + "');", function(err, rows, fields) {
-	  if (err) throw err;
-	  console.log('The solution is: ', rows[0].solution);
-	});
+	  });
 
 	var userInfo = {
 	  user_id: "1",
